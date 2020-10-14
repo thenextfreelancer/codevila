cc96af117e74b55bb56768dac76fc267c1ff116e
Arpit Mishra <arpit.mishra@mycom-osi.com>
2020-10-15 00:16:08 +0530

    adding chrome driver

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	 * You can download latest driver from here:
 	 * {@link https://chromedriver.chromium.org/downloads}
 	 */
-	private static final String chromeDriverPath = "lib//driver//chromedriver.exe";
+	private static final String chromeDriverPath = "driver//chromedriver.exe";
 	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
